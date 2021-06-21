f98d999100da6e67ecea9ec57768e8e47d774930
Takayuki AKAMINE <takayuki.akamine@tier4.jp>
2021-06-21 20:22:42 +0900

    fixed thread number by 4

--- rclcpp_components/src/component_container_mt.cpp
+++ rclcpp_components/src/component_container_mt.cpp
@@ -22,7 +22,7 @@ int main(int argc, char * argv[])
 {
   /// Component container with a multi-threaded executor.
   rclcpp::init(argc, argv);
-  auto exec = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  auto exec = std::make_shared<rclcpp::executors::MultiThreadedExecutor>(rclcpp::ExecutorOptions(), 4);
   auto node = std::make_shared<rclcpp_components::ComponentManager>(exec);
   exec->add_node(node);
   exec->spin();
